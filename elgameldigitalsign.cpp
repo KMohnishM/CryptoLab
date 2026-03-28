0150c2741270cac73fd20c17c54096401eac137a
Kodukulla Mohnish Mythreya <kmohnishm@gmail.com>
2026-03-28 14:36:56 +0530

    Refactor to use C standard library and update I/O
    Refactor code to replace C++ standard library with C standard library functions, and modify prime-related functions for better handling. Update main function to use printf and scanf instead of cout and cin.

--- elgameldigitalsign.cpp
+++ elgameldigitalsign.cpp
@@ -1,29 +1,9 @@
-#include <iostream>
-#include <vector>
-#include <cstdlib>
-#include <ctime>
-using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-
-bool isPrime(int n) {
-    if (n < 2) return false;
-    for (int i = 2; i * i <= n; i++)
-        if (n % i == 0)
-            return false;
-    return true;
-}
-
-
-int findPrime4m1(int start) {
-    int p = start;
-    while (true) {
-        if (p % 4 == 1 && isPrime(p))
-            return p;
-        p++;
-    }
-}
-
-
+// Fast modular exponentiation
 long long power(long long base, long long exp, long long mod) {
     long long res = 1;
     base %= mod;
@@ -36,81 +16,158 @@ long long power(long long base, long long exp, long long mod) {
     return res;
 }
 
-
-vector<int> primeFactors(int n) {
-    vector<int> f;
-    for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0) {
-            f.push_back(i);
-            while (n % i == 0) n /= i;
-        }
+// GCD
+long long gcd(long long a, long long b) {
+    while (b) {
+        long long t = b;
+        b = a % b;
+        a = t;
     }
-    if (n > 1) f.push_back(n);
-    return f;
+    return a;
 }
 
+// Extended Euclidean Algorithm (mod inverse)
+long long modInverse(long long a, long long m) {
+    long long m0 = m, t, q;
+    long long x0 = 0, x1 = 1;
 
-vector<int> primitiveRoots(int p) {
-    vector<int> roots;
-    int phi = p - 1;
-    vector<int> factors = primeFactors(phi);
+    while (a > 1) {
+        q = a / m;
+        t = m;
+        m = a % m; a = t;
+        t = x0;
+        x0 = x1 - q * x0;
+        x1 = t;
+    }
 
-    for (int g = 2; g < p; g++) {
-        bool ok = true;
-        for (int f : factors) {
-            if (power(g, phi / f, p) == 1) {
-                ok = false;
+    if (x1 < 0)
+        x1 += m0;
+
+    return x1;
+}
+
+// Prime check
+int isPrime(long long n) {
+    if (n < 2) return 0;
+    for (long long i = 2; i * i <= n; i++)
+        if (n % i == 0) return 0;
+    return 1;
+}
+
+// Get next prime
+long long nextPrime(long long n) {
+    while (!isPrime(n)) n++;
+    return n;
+}
+
+// Get prime factors of n
+void getPrimeFactors(long long n, long long factors[], int *size) {
+    *size = 0;
+    for (long long i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            factors[(*size)++] = i;
+            while (n % i == 0)
+                n /= i;
+        }
+    }
+    if (n > 1)
+        factors[(*size)++] = n;
+}
+
+// Find primitive root
+long long primitiveRoot(long long p) {
+    long long phi = p - 1;
+    long long factors[20];
+    int size;
+
+    getPrimeFactors(phi, factors, &size);
+
+    for (long long g = 2; g < p; g++) {
+        int flag = 1;
+        for (int i = 0; i < size; i++) {
+            if (power(g, phi / factors[i], p) == 1) {
+                flag = 0;
                 break;
             }
         }
-        if (ok) roots.push_back(g);
+        if (flag)
+            return g;
     }
-    return roots;
+    return -1;
+}
+
+// Simple hash (sum of ASCII)
+long long hashMessage(char *msg) {
+    long long h = 0;
+    for (int i = 0; i < strlen(msg); i++)
+        h += msg[i];
+    return h;
 }
 
 int main() {
-    long long roll;
-    cout << "Enter Roll Number: ";
-    cin >> roll;
+    long long start, p, g, x, y, k, r, s;
+    char name[100];
 
-    int last4 = roll % 10000;
-    int p = findPrime4m1(last4);
+    printf("Enter starting number (reg no): ");
+    scanf("%lld", &start);
+    getchar();
 
-    cout << "\nLast 4 digits: " << last4;
-    cout << "\nPrime (4m + 1) chosen: p = " << p << endl;
+    printf("Enter your name: ");
+    fgets(name, sizeof(name), stdin);
+    name[strcspn(name, "\n")] = 0;
 
-    vector<int> roots = primitiveRoots(p);
+    // Generate prime and primitive root
+    p = nextPrime(start);
+    g = primitiveRoot(p);
 
-    cout << "\nPrimitive Roots of " << p << ":\n";
-    for (int g : roots) cout << g << " ";
-    cout << endl;
-
+    // Private key (random)
     srand(time(0));
-    int g = roots[0];
-    cout << "\nRandomly selected primitive root g = " << g << endl;
+    x = rand() % (p - 2) + 1;
 
-    int a, b;
-    cout << "\nEnter Alice private key a: ";
-    cin >> a;
-    cout << "Enter Bob private key b: ";
-    cin >> b;
+    // Public key
+    y = power(g, x, p);
 
-    long long A = power(g, a, p);
-    long long B = power(g, b, p);
+    // Hash message
+    long long m = hashMessage(name) % (p - 1);
 
-    cout << "\nAlice sends A = " << A;
-    cout << "\nBob sends B = " << B << endl;
+    // Choose random k
+    do {
+        k = rand() % (p - 2) + 1;
+    } while (gcd(k, p - 1) != 1);
 
-    long long s1 = power(B, a, p);
-    long long s2 = power(A, b, p);
+    // Signature
+    r = power(g, k, p);
+    long long k_inv = modInverse(k, p - 1);
 
-    cout << "\nShared Secret (Alice): " << s1;
-    cout << "\nShared Secret (Bob): " << s2 << endl;
+    s = (k_inv * (m - x * r)) % (p - 1);
+    if (s < 0) s += (p - 1);
 
-    if (s1 == s2)
-        cout << "\nKey Exchange Successful ";
+    printf("\n--- Generated Values ---\n");
+    printf("Prime (p): %lld\n", p);
+    printf("Primitive Root (g): %lld\n", g);
+
+    printf("\n--- Keys ---\n");
+    printf("Private Key (x): %lld\n", x);
+    printf("Public Key (p, g, y): (%lld, %lld, %lld)\n", p, g, y);
+
+    printf("\n--- Message ---\n");
+    printf("Name: %s\n", name);
+    printf("Hash (m): %lld\n", m);
+
+    printf("\n--- Signature ---\n");
+    printf("(r, s): (%lld, %lld)\n", r, s);
+
+    // Verification
+    long long v1 = (power(y, r, p) * power(r, s, p)) % p;
+    long long v2 = power(g, m, p);
+
+    printf("\n--- Verification ---\n");
+    printf("v1 = %lld\nv2 = %lld\n", v1, v2);
+
+    if (v1 == v2)
+        printf("Verification Status: SUCCESS \n");
     else
-        cout << "\nKey Exchange Failed ";
+        printf("Verification Status: FAILED \n");
 
     return 0;
 }
